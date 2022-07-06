65707f5a550d6e95af663de28f9ac271a4452a41
Relidis <korel25@yandex.ru>
2022-07-06 23:24:31 +0300

    version 3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -10,12 +10,16 @@ public class AnimalsCage {
     @Autowired
     @Qualifier("dog")
     private Animal animal;
-
+    @Autowired
+    private Timer timer;
+    public Timer getTimer() {
+        return timer;
+    }
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
